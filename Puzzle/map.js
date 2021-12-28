6b07e5eb0649d1cfe18510e00c20883a47fc646d
mjuc <michal.jucewicz@protonmail.com>
2021-12-28 09:51:57 +0100

    Included leaflet plugin for geolocation

---
+++ Puzzle/map.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWp1YyIsImEiOiJja3hwNWZmcGYwNmk2MndvMnViaHc0dW5uIn0.jNnzoURf9jQtzfh7cXKSDw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserHeading: true
+    })
+);
